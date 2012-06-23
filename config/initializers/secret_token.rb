8898590d199f3759a17d1fe792b0d9e20642e92f
Rai <zclinux@gmail.com>
2012-06-23 18:20:08 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PharmaNewApp::Application.config.secret_token = '37e0e7767209dce50b227ac2e1264fd079ed0872b49cec5e680811f65190844890961d6d7a993ccbd2a1720607989a545ccec70745d0a705c1a8c4182e24b969'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+PharmaNewApp::Application.routes.draw do
+  root :to => "home#index"
+end
