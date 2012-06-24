3e118dc1c715b69ca4f394095c262edb508c29c6
Rai <zclinux@gmail.com>
2012-06-23 23:58:19 -0400

    heroku test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,9 @@ PharmaNewApp::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
+  # For heroku
+  config.assets.initialize_on_precompile = false
+
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
