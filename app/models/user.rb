4c61a41f43cfecb4c079f74af351019c201bcffc
Rai <zclinux@gmail.com>
2012-06-23 22:38:53 -0400

    add css changes and new views

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   attr_accessible :address, :dob, :email, :name, :phone, :insurance
 
   has_attached_file :insurance
+	
+=begin
+	  :styles=> {
+	      :thumbnail  => '30x30',
+	      :medium     => '200x200'
+	    }
+=end
 end
